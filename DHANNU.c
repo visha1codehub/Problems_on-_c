83bed96159773cacfe5154c6248f998740942602
Vishal Gupta <itsvishalhack@gmail.com>
2022-03-08 21:09:55 +0530

    Names Coding

---
+++ DHANNU.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if ((j == 0) || (j == 6 && (i != 0 && i != 6)) || (i == 0 && j != 6) || (i == 6 && j != 6))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (j == 0 || j == 6 || i == 3)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (((j == 0 || j == 6) && (i != 0)) || i == 3 || (i == 0 && (j != 0 && j != 6)))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (j == 0 || j == 6 || i - j == 0)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (j == 0 || j == 6 || i - j == 0)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (((j == 0 || j == 6) && (i != 6)) || (i == 6 && (j != 0 && j != 6)))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ SARIKA.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if ((((i == 0) || (i == 3) || (i == 6)) && ((j > 0) && (j < 6))) || ((j == 0) && ((i > 0) && (i < 3))) || ((j == 6) && ((i > 3) && (i < 6))))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (((j == 0 || j == 6) && (i != 0)) || i == 3 || (i == 0 && (j != 0 && j != 6)))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (((j == 0 || (j == 6 && (i > 0 && i < 3))) && (i != 0)) || (i == 3 && j != 6) || (i == 0 && j != 6) || (i - j == 1 && i > 3))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (((i == 0 || i == 6) && (j != 0 && j != 6)) || (j == 3))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (j == 0 || i + j == 4 || i - j == 2)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        for (int j = 0; j < 7; j++)
+        {
+            if (((j == 0 || j == 6) && (i != 0)) || i == 3 || (i == 0 && (j != 0 && j != 6)))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
