e37a255230b312d9600d2784faeaa4dccf436cde
Vishal Gupta <itsvishalhack@gmail.com>
2022-03-11 09:15:41 +0530

    studying

--- student_info.c
+++ student_info.c
@@ -4,11 +4,11 @@ typedef struct student_info
     char name[30];
     int roll_no;
     float marks;
-}std;
+}student;
 
 int main()
 {
-    std sns[50];
+    student std[50];
     int n;
 
     printf("Enter the total no. of students : ");
@@ -18,19 +18,30 @@ int main()
     {
         printf("Enter the imformation of student %d :\n",n);
         printf("Enter name: ");
-        scanf("%s",sns[i].name);
+        scanf("%s",std[i].name);
         printf("Enter roll no.: ");
-        scanf("%d",&sns[i].roll_no);
+        scanf("%d",&std[i].roll_no);
         printf("Enter marks: ");
-        scanf("%f",&sns[i].marks);
+        scanf("%f",&std[i].marks);
     }
     printf("Displaying information....\n");
     for (int i = 1; i <= n; i++)
     {
         printf("\nImformation of student %d:\n",n);
-        printf("Name: %s\n", sns[i].name);
-        printf("Roll no.: %d\n", sns[i].roll_no);
-        printf("Marks: %f\n", sns[i].marks);
+        printf("Name: %s\n", std[i].name);
+        printf("Roll no.: %d\n", std[i].roll_no);
+        printf("Marks: %f\n", std[i].marks);
     }
+    printf("List of those students who scored above 60%% marks.\n[ ");
+    for (int i = 1; i <= n; i++)
+    {
+        if (std[i].marks > 60)
+        {
+            printf("%s ",std[i].name);
+        }
+        
+    }
+    printf("]\n");
+    
     return 0;
 }
